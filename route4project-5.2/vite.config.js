3934f59aa785a20aacd2c86b8e5df199888c3744
Your Name <you@example.com>
2026-04-01 10:54:22 -0700

    trying again.

--- route4project-5.2/vite.config.js
+++ route4project-5.2/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/finalcapstone-draft1/',
+  base: '/finalcapstone-draft1 /',
 })
