8df15d144f91bdbd0889f348128cb6fe05dd494c
Your Name <you@example.com>
2026-04-01 10:44:53 -0700

    tryingagain

--- route4project-5.2/vite.config.js
+++ route4project-5.2/vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/route4project-5.2/',
+  base: '/Route for Project 5.2/',
 })
